7d526bfed95997c9a37a5acd622d1b5b8105095b
Prashant saini <64362251+Prashantsaini25@users.noreply.github.com>
2021-05-04 00:08:24 +0530

    Update setup_file.sh

--- setup_file.sh
+++ setup_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt-get -y install git
-git clone https://github.com/Prashantsaini25/prometheus_grafana_setup_with_shell_scripting.git
-chmod +x ./prometheus_grafana_setup_with_shell_scripting/*.sh
-./prometheus_grafana_setup_with_shell_scripting/prometheus-ubuntu.sh
-./prometheus_grafana_setup_with_shell_scripting/node-exporter-ubuntu.sh
-./prometheus_grafana_setup_with_shell_scripting/grafana-ubuntu.sh
-./prometheus_grafana_setup_with_shell_scripting/nginx-ubuntu.sh
+git clone https://github.com/Prashantsaini25/Prometheus_Grafana_Setup_with_shell_scripting.git
+chmod +x ./Prometheus_Grafana_Setup_with_shell_scripting/*.sh
+./Prometheus_Grafana_Setup_with_shell_scripting/prometheus-ubuntu.sh
+./Prometheus_Grafana_Setup_with_shell_scripting/node-exporter-ubuntu.sh
+./Prometheus_Grafana_Setup_with_shell_scripting/grafana-ubuntu.sh
+./Prometheus_Grafana_Setup_with_shell_scripting/nginx-ubuntu.sh
