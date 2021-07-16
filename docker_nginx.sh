8ceb4da1b46cbf5cedf99aabc7ac1b44ee819691
Prashant saini <64362251+Prashantsaini25@users.noreply.github.com>
2021-07-16 11:16:54 +0530

    Create docker_nginx.sh

---
+++ docker_nginx.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+sudo apt update
+sudo apt-get -y install nginx
+cat <<EOF | sudo tee /etc/nginx/sites-available/default
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                proxy_pass http://localhost:80;
+        }
+}
+EOF
+sudo systemctl restart nginx
+
+sudo apt-get install -y  apt-transport-https ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
